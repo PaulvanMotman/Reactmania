6917df240d6cf6ae7e5f4d2f401b47ddaba30b87
Paul van Motman <pcvanmotman@gmail.com>
2016-10-12 16:50:30 +0200

    created js file schoolsStore to add and delete schools + updated main.jsx

--- tutorialuno/app/main.jsx
+++ tutorialuno/app/main.jsx
@@ -1,9 +1,12 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var SchoolsList = require("./components/SchoolList.jsx");
+var SchoolsList = require("./components/SchoolsList.jsx");
+var schoolsStore = require("./stores/schoolsStore");
+var _schools = schoolsStore.getSchools();
+schoolsStore.onChange(function(schools){
+    _schools = schools;
+    render();
+});
 
-var schools = [{name:"Lovedale",tagline:"this is a wonderful school"},
-                {name:"Bishop",tagline:"Another great school"}];
-                
 
-ReactDOM.render(<SchoolsList schools={schools} />, document.getElementById("container"));    
+ReactDOM.render(<SchoolsList schools={_schools} />, document.getElementById("container"));    
