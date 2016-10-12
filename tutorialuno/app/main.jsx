26f1545417b45608921ae58fc6359e6253bd911a
Paul van Motman <pcvanmotman@gmail.com>
2016-10-12 17:31:35 +0200

    finished tutorial which, on to the next

--- tutorialuno/app/main.jsx
+++ tutorialuno/app/main.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require("react-dom");
 var SchoolsList = require("./components/SchoolList.jsx");
 var schoolsStore = require("./stores/schoolsStores");
 var _schools = schoolsStore.getSchools();
+
 schoolsStore.onChange(function(schools){
     _schools = schools;
     render();
