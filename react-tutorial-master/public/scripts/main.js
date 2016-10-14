0cc248f590965f6a11206fced09913fb55c9e06b
Paul van Motman <pcvanmotman@gmail.com>
2016-10-14 14:17:31 +0200

    Added commentlist, commentform and comment component. Then I included those within the commentbox component to render them all

--- react-tutorial-master/public/scripts/main.js
+++ react-tutorial-master/public/scripts/main.js
@@ -1,14 +1,63 @@
-// tutorial1.js
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var Comment = React.createClass({
+	rawMarkup: function() {
+		var md = new Remarkable();
+		var rawMarkup = md.render(this.props.children.toString());
+		return { __html: rawMarkup };
+	},
+	render: function() {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+			</div>
+		);
+	}
 });
+
+
+var CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+			<h1>Comments</h1>
+			<CommentList />
+			<CommentForm />
+			</div>
+		);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function() {
+		return (
+			<div className="commentList">
+				<Comment author="Pete Hunt">This is one comment</Comment>
+        		<Comment author="Jordan Walke">This is *another* comment</Comment>
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function() {
+		return (
+			<div className="commentForm">
+				Hello, world! I am a Commentbox.
+			</div>
+		);
+	}
+});
+
+
+
 ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
+	<CommentBox />, 
+	document.getElementById('content')
 );
