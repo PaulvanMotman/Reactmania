57c8cc670fdb65071ea46101d7023ba6018218f0
Paul van Motman <pcvanmotman@gmail.com>
2016-10-14 14:37:55 +0200

    added data within main.js and imported the comments from there using props

--- react-tutorial-master/public/scripts/main.js
+++ react-tutorial-master/public/scripts/main.js
@@ -27,7 +27,7 @@ var CommentBox = React.createClass({
 		return (
 			<div className="commentBox">
 			<h1>Comments</h1>
-			<CommentList />
+			<CommentList data={this.props.data} />
 			<CommentForm />
 			</div>
 		);
@@ -36,10 +36,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
 	render: function() {
+		var commentNodes = this.props.data.map(function(comment) {
+			return (
+				<Comment author={comment.author} key={comment.id}>
+					{comment.text}
+				</Comment>
+			);
+	    });
 		return (
 			<div className="commentList">
-				<Comment author="Pete Hunt">This is one comment</Comment>
-        		<Comment author="Jordan Walke">This is *another* comment</Comment>
+				{commentNodes}
 			</div>
 		);
 	}
@@ -58,6 +64,6 @@ var CommentForm = React.createClass({
 
 
 ReactDOM.render(
-	<CommentBox />, 
+	<CommentBox data={data} />, 
 	document.getElementById('content')
 );
