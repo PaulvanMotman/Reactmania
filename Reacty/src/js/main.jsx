5f243801acbbd50bd003b056c95bb5938686bd8a
Paul van Motman <pcvanmotman@gmail.com>
2016-10-19 17:42:18 +0200

    played around with props on different components

--- Reacty/src/js/main.jsx
+++ Reacty/src/js/main.jsx
@@ -1,1 +1,19 @@
-ReactDOM.render(<h1>joeehoeeee</h1>, document.getElementById('example'))
+class Movie extends React.Component {
+  render() {
+    return (
+    	<div>
+    		<h2>{this.props.title}</h2>
+    		<h3>{this.props.genre}</h3>
+    	</div>
+    )
+  }
+}
+
+ReactDOM.render(
+	<div>
+		<Movie title='avatar' genre='action movie'/>
+		<Movie title='titanic' genre='romance'/>
+		<Movie title='rush hour' genre='bromance'/>
+	</div>, 
+	document.getElementById('example')
+)
