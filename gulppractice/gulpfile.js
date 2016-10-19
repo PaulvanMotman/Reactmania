3f515389af2d92c6ac8691bdcc5c99d6f4875625
Paul van Motman <pcvanmotman@gmail.com>
2016-10-19 13:05:33 +0200

    minifying both css and js files now

--- gulppractice/gulpfile.js
+++ gulppractice/gulpfile.js
@@ -2,15 +2,38 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
 
 
+
+// browserSync reloads the browsers when changes are made
 gulp.task('browserSync', function() {
   browserSync.init({
+  	//setup a webserver that listens to file changes in this folder
     server: {
       baseDir: 'app'
     },
   })
 })
+
+
+// Gulp-useref concatenates any number of CSS and JavaScript 
+// files into a single file by looking for a comment that starts 
+// with "<!--build:" and ends with "<!--endbuild-->".
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+     // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+
 
 
 gulp.task('sass', function() {
@@ -21,6 +44,8 @@ gulp.task('sass', function() {
       stream: true
     }))
 });
+
+///gulp watch
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
