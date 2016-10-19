895069e439bd9c72ca342a97384fe38663410d71
Paul van Motman <pcvanmotman@gmail.com>
2016-10-19 12:35:05 +0200

    added browserSync and useref to gulpfile

--- gulppractice/gulpfile.js
+++ gulppractice/gulpfile.js
@@ -1,9 +1,30 @@
 var gulp = require('gulp');
-
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/styles.scss')  // take these files from this source directory
+  return gulp.src('app/scss/**/*.scss')  // take these files from this source directory
     .pipe(sass())   // compile these file into sass
     .pipe(gulp.dest('app/css')) // put these files in this directory
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
+
+gulp.task('watch', ['browserSync', 'sass'], function(){
+  gulp.watch('app/scss/**/*.scss', ['sass']); 
+  // Other watchers
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+})
