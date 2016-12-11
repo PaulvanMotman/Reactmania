4d56cc59225fdd251dd4f2e4c99bf4b0f2cd96a8
Paul van Motman <pcvanmotman@gmail.com>
2016-12-11 18:03:09 +0100

    latest version game

--- anime/custom.js
+++ anime/custom.js
@@ -1,13 +1,60 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCbc50cjw_RxosvWlHYNried8p3FkCkJu8",
+    authDomain: "klik-da5aa.firebaseapp.com",
+    databaseURL: "https://klik-da5aa.firebaseio.com",
+    storageBucket: "klik-da5aa.appspot.com",
+    messagingSenderId: "345960366975"
+}
+
+firebase.initializeApp(config);
+var database = firebase.database();
+
+
+function writeNewScore(username, score) {
+    // A post entry.
+    var userData = {
+      username: username,
+      score: score
+    };
+
+    // Get a key for a new Post.
+    var newUserKey = firebase.database().ref().child('users').push().key;
+
+    // Write the new post's data simultaneously in the posts list and the user's post list.
+    var updates = {};
+    updates['/users/' + newUserKey] = userData;
+
+    return firebase.database().ref().update(updates);
+}
+
+
+firebase.database().ref('/users/').once('value').then(function(snapshot) {
+    var data = snapshot.val()
+
+    for (var key in data) {
+        if (data.hasOwnProperty(key)) {
+            $( "#highscores" ).append( "<p>" + data[key].username + ": " + data[key].score + "</p>" )
+        }
+    }
+})
+
+
+
 var path = anime.path('#path1');
 var path2 = anime.path('#path2');
 var path3 = anime.path('#path3');
 var path4 = anime.path('#path4');
 
-var lasttime = 0
-var now = 0
-
 var score = 100
-var time = 1
+
+var topScore = {
+    users: [],
+    scores: []
+}
+
+var currentUser
+
 
 function blueAnimation (id) {
     return  anime({
@@ -16,15 +63,15 @@ function blueAnimation (id) {
         translateY: path2,
         rotate: {
             value: 2040,
-            duration: 5000,
+            duration: 12000,
             easing: 'easeInOutQuad'
         },
         scale: {
             value: 0.5,
-            duration: 5000,
+            duration: 12000,
             easing: 'easeOutBounce'
         },
-        duration: 5000,
+        duration: 12000,
         loop: true,
         easing: 'linear',
         autoplay: false,
@@ -38,15 +85,15 @@ function redAnimation (id) {
         translateY: path3,
         rotate: {
             value: 1020,
-            duration: 5000,
+            duration: 8000,
             easing: 'easeInOutQuad'
         },
         scale: {
             value: 2,
-            duration: 5000,
+            duration: 8000,
             easing: 'easeOutBounce'
         },
-        duration: 5000,
+        duration: 8000,
         loop: true,
         easing: 'linear',
         autoplay: false,
@@ -60,15 +107,15 @@ function greenAnimation (id) {
         translateY: path4,
         rotate: {
             value: 2040,
-            duration: 3000,
+            duration: 6000,
             easing: 'easeInOutQuad'
         },
         scale: {
             value: 4,
-            duration: 3000,
+            duration: 6000,
             easing: 'easeOutBounce'
         },
-        duration: 3000,
+        duration: 6000,
         loop: true,
         easing: 'linear',
         autoplay: false,
@@ -97,9 +144,6 @@ function yellowAnimation (id) {
     });
 }
 
-// Create timer
-// 
-
 
 function isFalse(element, index, array) { 
     return element.check == false; 
@@ -115,24 +159,24 @@ for (var i = 16 - 1; i >= 0; i--) {
     }
 }
 
-console.log(clicked)
 
 
 function click (click, animation) {
     if (!clicked[click].check) {
-        console.log("hoi")
-        console.log(clicked[click])
         clicked[click].check = true
         animation.play()
     } else {
-        console.log("doei")
         clicked[click].check = false
+        clicked[click].number++
         animation.pause()
-        score += 10
+        if (clicked[click].number < 2) {
+            score += 10
+        }
         $('#score').html(score.toString())
-
         if (clicked.every(isFalse)) {
+            writeNewScore(currentUser, score)
             alert("You did it! You're score is " + score)
+            window.location.href = "https://www.paulvanmotman.com/game/index.html"
         }
     }
 }
@@ -146,20 +190,25 @@ function generate_clickfunction(i, j) {
     };
 }
 
-for (var i = animations.length -1; i >= 0; i--) {
-    $(selectors[i]).click(generate_clickfunction(i,i))          
-}
 
-$('#start').click( function () {
+$('#start').submit(function(e) {
 
-    $(this).attr("disabled", 'disabled')
+    for (var i = animations.length -1; i >= 0; i--) {
+        $(selectors[i]).click(generate_clickfunction(i,i))          
+    }
 
-    lasttime = Date.now()/1000
+    e.preventDefault()
+
+    $('#button').attr("disabled", 'disabled')
+
+    currentUser = $('#username').val()
+    console.log(currentUser)
+
+
     for (var i = animations.length -1, j = 6400 ; i >= 0, j >= 400; i--, j -= 400) {
         setTimeout(generate_clickfunction(i, i), j)   
     }
 
-    $('#time').html('0:00')
     $('#score').html('100')
    
 
@@ -169,16 +218,9 @@ $('#start').click( function () {
         if (score == 0) {
             alert("Game over!")
             $("#container").hide()
-            window.location.href = "file:///Users/paulvanmotman/dev/Reactmania/anime/index.html";
+            window.location.href = "https://www.paulvanmotman.com/game/index.html"
         }
     }, 1000)
-
-    setInterval( function () {
-        $('#time').html(time.toString() + ':00')
-        time++
-    }, 1000)
-
-   
 
 })
 
